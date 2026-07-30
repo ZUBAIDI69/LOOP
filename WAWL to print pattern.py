b509c965eb7734c0b12121260a734a7e90307c0d
ZUBAIDI69 <nafiz7209@gmail.com>
2026-07-30 21:56:18 +0600

    Add files via upload

---
+++ WAWL to print pattern.py
@@ -0,0 +1,20 @@
+#pattern 1
+n = 5
+i = 1
+while i <= n:
+    print(n * "*")
+    i += 1
+print()
+
+#pattern 2
+i = 1
+while i <= 5 :
+    print(i * "*")
+    i += 1
+print()
+
+#pattern 3
+i = 5
+while i >= 1 :
+    print(i * "*")
+    i -= 1 
