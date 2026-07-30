a57b12506fa5aa30981020a68ed738affde7cb0c
ZUBAIDI69 <nafiz7209@gmail.com>
2026-07-30 22:18:54 +0600

    Add files via upload

---
+++ WAFL to print patterns.py
@@ -0,0 +1,19 @@
+#pattern 1
+for i in range (1,6):
+    for j in range(1,6):
+        print("*",end=' ')
+    print()
+#pattern 2
+
+for i in range (1,6):
+    for j in range (1,i+1):
+        print("*",end=" ")
+    print()
+print()
+#pattern 3
+for i in range (5,0,-1):
+    for j in range(1,i):
+        print("*",end=" ")
+    print()
+
+    
