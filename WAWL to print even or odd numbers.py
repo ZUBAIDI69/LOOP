3ba82b2608f0e1ef8a65586355fefc5eb1d5e950
ZUBAIDI69 <nafiz7209@gmail.com>
2026-07-31 21:25:23 +0600

    Add files via upload

---
+++ WAWL to print even or odd numbers.py
@@ -0,0 +1,15 @@
+#even numbers 1~100
+n = 100
+i = 1
+while i <= n:
+    if i % 2 == 0:
+        print(i)
+    i+=1
+    
+#odd numbers
+n = 100
+i = 1
+while i <= n:
+    if i % 2 == 1:
+        print(i)
+    i+=1
